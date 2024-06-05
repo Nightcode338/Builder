0eb8b7c878dadbdbabe2df45e973a3b91dc8390b
Nightcode338 <abdulrasheedbello338@gmail.com>
2024-06-05 20:49:57 +0100

    i used the first design i sent to you.

--- Resume_Builder/src/App.jsx
+++ Resume_Builder/src/App.jsx
@@ -4,11 +4,7 @@ import Home from "./components/pages/Home"
 import Template from "./components/pages/Template";
 import Tabulio from "./components/pages/Tabulio";
 import About from "./components/pages/About";
-<<<<<<< HEAD
-import Login from "./components/Auth/Login";
-=======
-import Login from "../src/components/auth/Login";
->>>>>>> 71ee28c6e9b5c8480024b52619a06625889bd9ca
+import Login from "../src/components/Auth/Login";
 
 
 const App = () => {
@@ -17,11 +13,10 @@ const App = () => {
       <BrowserRouter>
         <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="Login" element={<Login/>}/>
         <Route path="template" element={<Template />} />
         <Route path="join" element={<Tabulio />} />
         <Route path="about" element={<About />} />
-        <Route path="login" element={<Login />} />
+        <Route path="Login" element={<Login />} />
         </Routes>
 
       </BrowserRouter>
